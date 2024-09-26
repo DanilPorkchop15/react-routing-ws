2ce29b62c93cfda414b51b0877635694056b3b3a
Danil Kostin <danilkostin150705@gmail.com>
2024-09-26 13:10:40 +0300

    feat(routing): modified all to be more visualising

--- v5/src/App.tsx
+++ v5/src/App.tsx
@@ -7,30 +7,53 @@ import { EntityPage } from "./pages/EntityPage/EntitePage";
 import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
 import { Redirect } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import { ENTITY_LIST } from "./entities";
 
 function App() {
   const [authed, setAuthed] = React.useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <NavLink to="/" className="App-link">
-          Home
-        </NavLink>
-        <NavLink to="/authed" className="App-link">
-          Authed Only
-        </NavLink>
-        <button
-          onClick={() => {
-            setAuthed(!authed);
-          }}
-        >
-          {authed ? "Logout" : "Login"}
-        </button>
+        <div className="App-header-block">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="App-link-active"
+            className="App-link"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/authed"
+            activeClassName="App-link-active"
+            className="App-link"
+          >
+            Authed Only
+          </NavLink>
+          <button
+            onClick={() => {
+              setAuthed(!authed);
+            }}
+          >
+            {authed ? "Logout" : "Login"}
+          </button>
+        </div>
+        <div className="App-header-block">
+          {ENTITY_LIST.map((entity) => (
+            <NavLink
+              key={entity.id}
+              to={`/entity/${entity.id}`}
+              activeClassName="App-link-active"
+              className="App-link"
+            >
+              {entity.name}
+            </NavLink>
+          ))}
+        </div>
       </header>
 
       <Switch>
         <Route path="/" exact component={EntityListPage} />
-        <Route path="/entity" exact component={EntityListPage} />
         <Route path="/authed">
           {authed ? <AuthedOnlyPage /> : <Redirect to="/" />}
         </Route>
