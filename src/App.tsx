ef9361c107db9cb12b406d3b3c1af0605d230b6d
Danil Kostin <danilkostin150705@gmail.com>
2024-09-26 12:25:22 +0300

    feat(routing): made routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { EntityListPage } from "./pages/EntityListPage/EntityListPage";
+import { AuthedOnlyPage } from "./pages/AuthedOnlyPage/AuthedOnlyPage";
+import { EntityPage } from "./pages/EntityPage/EntitePage";
+import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
+import { Navigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function App() {
+  const [authed, setAuthed] = React.useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavLink to="/" className="App-link">Home</NavLink>
+        <NavLink to="/authed" className="App-link">Authed Only</NavLink>
+        <button onClick={() => {setAuthed(!authed)}}>
+          {authed ? "Logout" : "Login"}
+        </button>
       </header>
+
+      <Routes>
+        <Route path="/" element={<EntityListPage />} />
+        <Route path="/entity" element={<EntityListPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+        <Route
+          path="/authed"
+          element={authed ? <AuthedOnlyPage /> : <Navigate to="/" replace />}
+        />
+        <Route path="/entity/:entityId" element={<EntityPage />} />
+      </Routes>
     </div>
   );
 }
