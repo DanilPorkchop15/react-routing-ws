344d31b0ccbdc3988b5e1bd5ed34d72477a5161c
Danil Kostin <danilkostin150705@gmail.com>
2024-09-26 13:10:21 +0300

    feat(routing): modified all to be more visualising

--- v6/src/App.tsx
+++ v6/src/App.tsx
@@ -7,17 +7,51 @@ import { EntityPage } from "./pages/EntityPage/EntitePage";
 import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
 import { Navigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import { ENTITY_LIST } from "./entities";
 
 function App() {
   const [authed, setAuthed] = React.useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <NavLink to="/" className="App-link">Home</NavLink>
-        <NavLink to="/authed" className="App-link">Authed Only</NavLink>
-        <button onClick={() => {setAuthed(!authed)}}>
-          {authed ? "Logout" : "Login"}
-        </button>
+        <div className="App-header-block">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `App-link ${isActive ? "App-link-active" : ""}`
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/authed"
+            className={({ isActive }) =>
+              `App-link ${isActive ? "App-link-active" : ""}`
+            }
+          >
+            Authed Only
+          </NavLink>
+          <button
+            onClick={() => {
+              setAuthed(!authed);
+            }}
+          >
+            {authed ? "Logout" : "Login"}
+          </button>
+        </div>
+        <div className="App-header-block">
+          {ENTITY_LIST.map((entity) => (
+            <NavLink
+              key={entity.id}
+              to={`/entity/${entity.id}`}
+              className={({ isActive }) =>
+                `App-link ${isActive ? "App-link-active" : ""}`
+              }
+            >
+              {entity.name}
+            </NavLink>
+          ))}
+        </div>
       </header>
 
       <Routes>
